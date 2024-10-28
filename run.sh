8d5b4d266bdc5474a5eb9263122f8c4d9f661138
rushikesh.softvan@gmail.com <rushikesh.softvan@gmail.com>
2024-10-28 22:38:38 +0530

    run script

--- run.sh
+++ run.sh
@@ -3,6 +3,8 @@
 # Exit immediately if a command exits with a non-zero status
 set -e
 
+git pull
+
 # Define variables
 DOCKER_IMAGE_NAME="your-docker-image-name"  # Replace with your desired Docker image name
 
