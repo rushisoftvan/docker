05060a8e0d5dd570a2b4a0ade215dfdd26a7ecbb
rushikesh.softvan@gmail.com <rushikesh.softvan@gmail.com>
2024-10-28 22:27:43 +0530

    run script

---
+++ run.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Exit immediately if a command exits with a non-zero status
+set -e
+
+# Define variables
+DOCKER_IMAGE_NAME="your-docker-image-name"  # Replace with your desired Docker image name
+
+# Build the Maven project
+echo "Building the Maven project..."
+call ./mvnw clean package
+
+docker-compose up --build
+
+
